4996e7eea1a84df590ac82b8c8bbff5495b781b8
destx0 <destisdbest@gmail.com>
2024-10-05 15:56:51 +0530

    emptyr type

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,9 +1,15 @@
+import React from 'react'
+
 import { UseChatHelpers } from 'ai/react'
 
 import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
-export function EmptyScreen() {
+interface EmptyScreenProps {
+  setInput: React.Dispatch<React.SetStateAction<string>>
+}
+
+export function EmptyScreen({ setInput }: EmptyScreenProps) {
   return <div className="mx-auto max-w-2xl px-4"></div>
 }
